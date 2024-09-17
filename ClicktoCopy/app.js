6384e75fb9295db3d9562700fdbf90898bce2800
Aphidech <aphidech12780@gmail.com>
2024-09-17 14:17:17 +0700

    Modified app.js

--- ClicktoCopy/app.js
+++ ClicktoCopy/app.js
@@ -1,11 +1,20 @@
 const btn = document.querySelector(".btn")
 const coupon = document.querySelector(".coupon")
 
+//check event click
 btn.addEventListener("click",(e) => {
     // console.log("click")
+    //select element
     coupon.select()
-    coupon.setSelectionRange(0,99999)
+    //set range inde 0 - 999999
+    coupon.setSelectionRange(0,999999)
+    //copy to clipboard
     navigator.clipboard.writeText(coupon.value)
+    //after click button change text
     btn.textContent="คัดลอกได้แล้ว"
-    
+    //set time after click 3 second
+    setTimeout( () => {
+        //change text after click 3 second
+        btn.textContent="คัดลอก"
+    },3000)
 })
