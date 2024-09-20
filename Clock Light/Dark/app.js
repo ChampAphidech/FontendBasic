ae62d361af57db34e10d359c7d94ebb36e39edda
Aphidech <aphidech12780@gmail.com>
2024-09-20 21:31:03 +0700

    New project Clocl dark/light

---
+++ Clock Light/Dark/app.js
@@ -0,0 +1,10 @@
+const timeEl = document.querySelector(".time")
+
+function setTime(){
+    const time = new Date()
+    const hours = time.getHours()
+     
+}
+setTime()
+setTime()
+setInterval(setTime,1500)
