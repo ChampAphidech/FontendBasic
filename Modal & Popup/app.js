0bd480053e29f64fbb7c03e0889f593387ed3f8b
Aphidech <aphidech12780@gmail.com>
2024-09-26 23:59:54 +0700

    modify app.js

--- Modal & Popup/app.js
+++ Modal & Popup/app.js
@@ -1,2 +1,11 @@
 const openbtn = document.querySelector(".open-btn")
 const closebtn = document.querySelector(".close-btn")
+const modalContainer = document.querySelector(".modal-container")
+
+openbtn.addEventListener("click", ()=> {
+    modalContainer.classList.add("show")
+})
+
+closebtn.addEventListener("click", ()=> {
+    modalContainer.classList.remove("show")
+})
