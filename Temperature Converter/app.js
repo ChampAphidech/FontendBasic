eb098b9f3770a86ee942b627b722f1d6ecb0e177
Aphidech <aphidech12780@gmail.com>
2024-10-16 22:43:33 +0700

    input temp celcius to fahrenheit and kelvin

--- Temperature Converter/app.js
+++ Temperature Converter/app.js
@@ -7,7 +7,7 @@ tempInput.forEach(input=>{
     input.addEventListener("input",(e)=>{
         let tempValue = parseInt(e.target.value)
         let inputName = e.target.name
-        console.log("temp ",tempValue,"unit ",inputName)
+        // console.log("temp ",tempValue,"unit ",inputName)
 
         //null value
         if (e.target.value === "") {
@@ -17,5 +17,14 @@ tempInput.forEach(input=>{
             return
         }
         //proocess change temp
+        if(inputName="celcius"){
+            //C to F
+            let fahrenheit = tempValue * 1.80 + 32
+            fahrenheitIn.value = fahrenheit.toFixed(2)
+
+             //C to K
+             let kelvin = tempValue + 273 
+             kelvinIn.value = kelvin.toFixed(2)
+        }
     })
 })
