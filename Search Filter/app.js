2e3712f32b9025d357f6f534e8d40444dbe4fe78
Aphidech <aphidech12780@gmail.com>
2024-10-17 23:17:56 +0700

    modify flie app.js to show data

--- Search Filter/app.js
+++ Search Filter/app.js
@@ -1,3 +1,6 @@
+const result = document.getElementById("result")
+const listItem = []
+
 //async ดึงข้อมูลมาให้ครบก่อนแสดงผล
 async function getData(){
     const url ="https://restcountries.com/v3.1/all"
@@ -5,6 +8,23 @@ async function getData(){
     const response =  await fetch(url)
     const item = await response.json()
     console.log(item);
+    result.innerHTML=""
+    item.forEach(data => {
+        const li = document.createElement("li")
+        listItem.push(li)
+        //show data from li
+        li.innerHTML=`
+        <div class="img-container">
+            <img src="${data.flags.svg}" />
+        </div>
+        <div class="info">
+            <h3>${data.name.common}</h3>
+            <p>Population : <span>${data.population}</span></p>
+            <p>Capital : <span>${data.capital}</span></p>
+        </div>
+        `
+        result.appendChild(li);
+    });
 
 }
 
