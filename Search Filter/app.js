9a1f0c5464eadf48207ac7e284721e1d304eab0d
Aphidech <aphidech12780@gmail.com>
2024-11-01 21:31:41 +0700

    modify function fiter js and style

--- Search Filter/app.js
+++ Search Filter/app.js
@@ -1,5 +1,20 @@
 const result = document.getElementById("result")
+const filter = document.getElementById("filter")
 const listItem = []
+
+filter.addEventListener("input",(e)=> {
+    const search = e.target.value.toLowerCase()
+    console.log(search)
+    listItem.forEach(item=>{
+        if(item.innerHTML.toLowerCase().includes(search)){
+            item.classList.add("highlight")
+            item.classList.remove("hide")
+        }else{
+            item.classList.add("hide")
+            item.classList.remove("highlight")
+        }
+    })
+})
 
 //async ดึงข้อมูลมาให้ครบก่อนแสดงผล
 async function getData(){
@@ -7,7 +22,7 @@ async function getData(){
     //await รอให้ดึงข้อมูลมาให้ครบค่อยทำอย่างอื่น
     const response =  await fetch(url)
     const item = await response.json()
-    console.log(item);
+    // console.log(item);
     result.innerHTML=""
     item.forEach(data => {
         const li = document.createElement("li")
@@ -19,7 +34,7 @@ async function getData(){
         </div>
         <div class="info">
             <h3>${data.name.common}</h3>
-            <p>Population : <span>${data.population}</span></p>
+            <p>Population : <span>${formatNumber(data.population)}</span></p>
             <p>Capital : <span>${data.capital}</span></p>
         </div>
         `
@@ -27,5 +42,10 @@ async function getData(){
     });
 
 }
+
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
+
 
 getData()
